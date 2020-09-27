cf0269b9d594a7ce36669dd465976db6e8680bfd
jindalakhil <akhiljindal222@gmail.com>
2020-09-28 00:07:18 +0530

    [Akhil Jidal]Updated: Take inputs from console

--- src/com/capg/Executor.java
+++ src/com/capg/Executor.java
@@ -1,13 +1,35 @@
 package com.capg;
+import java.io.*;
+import java.util.*;
 
 public class Executor {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Adress Book");
-		Contact contact = new Contact("first", "last","address","city","state",12345,"phone","email");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first name: ");
+		String fname = sc.nextLine();
+		System.out.println("Enter last name: ");
+		String lname = sc.nextLine();
+		System.out.println("Enter address: ");
+		String address = sc.nextLine();
+		System.out.println("Enter city: ");
+		String city = sc.nextLine();
+		System.out.println("Enter state: ");
+		String state = sc.nextLine();
+		System.out.println("Enter zip: ");
+		long zip = sc.nextLong();
+		System.out.println("Enter phone number: ");
+		sc.nextLine();
+		String phoneNumber = sc.nextLine();
+		System.out.println("Enter email: ");
+		String email = sc.nextLine();
 		AddressBook obj = new AddressBook();
+		Contact contact = new Contact(fname, lname, address, city, state, zip, phoneNumber, email);
 		obj.addContact(contact);
 		obj.viewBook(obj.getBook());
 	}
 
 }
+
+
