514f4640af84f325272d77f57bcbbf855dceca1c
jindalakhil <akhiljindal222@gmail.com>
2020-09-28 13:52:23 +0530

    [Akhil Jindal]Updated: For multiple contacts

--- src/com/capg/Executor.java
+++ src/com/capg/Executor.java
@@ -10,7 +10,6 @@ public class Executor {
 		AddressBook obj = new AddressBook();
 		Scanner sc = new Scanner(System.in);
 		int i = 1;
-		int contacts =0;
 		while (i != 0) {
 			String firstName = "";
 			System.out.println("Enter 1 to add a contact");
@@ -19,37 +18,28 @@ public class Executor {
 			System.out.println("Enter 0 to exit");
 			i = sc.nextInt();
 			sc.nextLine();
-			if (i > 2) {
-				System.out.println("Enter valid entry");
-			}
 			switch (i) {
 			case 1:
-				if(contacts<1) {
-					System.out.println("Enter the first name: ");
-					String fname = sc.nextLine();
-					System.out.println("Enter last name: ");
-					String lname = sc.nextLine();
-					System.out.println("Enter address: ");
-					String address = sc.nextLine();
-					System.out.println("Enter city: ");
-					String city = sc.nextLine();
-					System.out.println("Enter state: ");
-					String state = sc.nextLine();
-					System.out.println("Enter zip: ");
-					long zip = sc.nextLong();
-					System.out.println("Enter phone number: ");
-					sc.nextLine();
-					String phoneNumber = sc.nextLine();
-					System.out.println("Enter email: ");
-					String email = sc.nextLine();
-					Contact contact = new Contact(fname, lname, address, city, state, zip, phoneNumber, email);
-					obj.addContact(contact);
-					obj.viewBook(obj.getBook());
-					contacts++;
-				} else {
-					System.out.println("Add only one contact at this time");
-				}
-				
+				System.out.println("Enter the first name: ");
+				String fname = sc.nextLine();
+				System.out.println("Enter last name: ");
+				String lname = sc.nextLine();
+				System.out.println("Enter address: ");
+				String address = sc.nextLine();
+				System.out.println("Enter city: ");
+				String city = sc.nextLine();
+				System.out.println("Enter state: ");
+				String state = sc.nextLine();
+				System.out.println("Enter zip: ");
+				long zip = sc.nextLong();
+				System.out.println("Enter phone number: ");
+				sc.nextLine();
+				String phoneNumber = sc.nextLine();
+				System.out.println("Enter email: ");
+				String email = sc.nextLine();
+				Contact contact = new Contact(fname, lname, address, city, state, zip, phoneNumber, email);
+				obj.addContact(contact);
+				obj.viewBook(obj.getBook());
 				break;
 			case 2:
 				System.out.println("Update Contact:");
@@ -66,6 +56,9 @@ public class Executor {
 				break;
 			case 0:
 				System.out.println("Exiting the process");
+				break;
+			default:
+				System.out.println("Enter valid entry");
 			}
 		}
 
